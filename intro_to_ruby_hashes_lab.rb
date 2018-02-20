7d3b0d98f647e3baa4c0db158b078f36dbb16cdc
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-20 17:39:41 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,11 +21,18 @@ def monopoly_with_second_tier
 			:rent_in_dollars => {}
 		}
 	}
-
 end
 
 def monopoly_with_third_tier
-
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:names => {},
+			:rent_in_dollars => {
+				:one_piece_owned => 25
+			}
+		}
+	}
 end
 
 def monopoly_with_fourth_tier
